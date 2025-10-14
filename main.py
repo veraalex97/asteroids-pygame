d1c12ce61d627cf9ad446593f3924724d929a241
Alex Vera <veraalex97@gmail.com>
2025-10-13 22:40:45 -0400

    game loop

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     running = True
 
@@ -17,9 +19,8 @@ def main():
                 running = False
         screen.fill("black")
 
-
-        pygame.display.flip()
-
+    
+    dt = clock.tick(60) / 1000
     pygame.quit()
 
 if __name__ == "__main__":
